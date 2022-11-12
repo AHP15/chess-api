e3e271189ccb9e973d540e41e7c368b125a8a529
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-11-12 17:53:29 +0100

    Add test for signup controller

---
+++ __tests__/routes/user.js
@@ -0,0 +1,9 @@
+//import app from "../../app.js";
+/*
+describe("Test the signup route", () => {
+    test("It should response the POST method", async () => {
+        const response = await request(app).post("/api/v1/auth/signup");
+        expect(response.statusCode).toBe(201);
+    });
+});
+*/
